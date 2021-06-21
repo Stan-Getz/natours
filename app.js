acec8dc7734135adb6604797cbb2bbfc1636de3b
Stan-Getz <stanislav.genunchi@gmail.com>
2021-06-21 14:48:52 +0200

    Add access to static files

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(morgan('dev'));
 // Middleware (for adding data from the body to request)
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
