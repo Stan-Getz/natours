b5d76d3fc1fb97c355591b3eb93cb526f2b7f154
Stan-Getz <stanislav.genunchi@gmail.com>
2021-06-23 11:51:17 +0200

    Cleaned up the code

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,14 +3,12 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
+// router.param('id', tourController.checkID);
 
 // Create a checkbody middleware
 // Check if body contains the name and price property
 // If not, send back 400 (bad request)
 // Add it to the post handler stack
-
-router.param(('name', 'price'), tourController.checkBody);
 
 router
   .route('/')
--- server.js
+++ server.js
@@ -20,41 +20,6 @@ mongoose
     console.log('DB connection succesful!');
   });
 
-// Schema
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-// Model
-const Tour = mongoose.model('Tour', tourSchema);
-
-// Instance of a model
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR 💥:', err);
-  });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port: ${port}...`);
