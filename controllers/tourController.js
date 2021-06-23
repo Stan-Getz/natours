11ecc9a8b605a4532acc7dd662549738f72e06bd
Stan-Getz <stanislav.genunchi@gmail.com>
2021-06-23 14:40:04 +0200

    Improve delete doc. function

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -75,7 +75,6 @@ exports.updateTour = async (req, res) => {
       },
     });
   } catch (err) {
-    console.log(err);
     res.status(404).json({
       status: 'fail',
       message: err,
@@ -83,9 +82,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
