41f76e72ac5bd17fdcb3f068a4e23da5eaacf34e
Stan-Getz <stanislav.genunchi@gmail.com>
2021-06-24 19:04:47 +0200

    Added sorting by price and rating, default sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,13 +4,13 @@ exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
 
+    // 1A) Filtering
     // BUILD QUERY
-    // 1) FILTERING
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 2) ADVANCED FILTERING
+    // 1B) Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
@@ -20,7 +20,18 @@ exports.getAllTours = async (req, res) => {
     // gte, gt, lte, lt
 
     // 1ST OPTION OF WRITING A QUERY
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+      query = query.sort(sortBy);
+      //  Mongoose
+      //  sort('price ratingsAverage')
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // EXECUTE QUERY
     const tours = await query;
